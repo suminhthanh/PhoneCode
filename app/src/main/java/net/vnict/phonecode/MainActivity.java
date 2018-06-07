c12a4360a85baefda1160f8d3400ae4efcd9af5d
suminhthanh <smthanh@gmail.com>
2018-06-07 14:11:08 +0700

    Revert "* Fixed bug"
    This reverts commit f1e54b93bbe23f2750dc30fe75fb16e95f741ecc.

--- app/src/main/java/net/vnict/phonecode/MainActivity.java
+++ app/src/main/java/net/vnict/phonecode/MainActivity.java
@@ -282,6 +282,9 @@ public class MainActivity extends AppCompatActivity {
                 b.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        if (mInterstitialAd.isLoaded()) {
+                            mInterstitialAd.show();
+                        }
                         dialog.cancel();
                     }
                 });
